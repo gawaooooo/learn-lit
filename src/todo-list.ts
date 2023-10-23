4cdc0c8a01096b2f8d586aa54438f1834ba227fc
gawaooooo <gawao57@gmail.com>
2023-10-23 16:00:24 +0900

    feat: checkの状態を判別してStyleを変更する

--- src/todo-list.ts
+++ src/todo-list.ts
@@ -1,6 +1,6 @@
-import { LitElement, html } from 'lit';
-import { customElement, state } from 'lit/decorators.js';
-import { map } from 'lit/directives/map.js';
+import {LitElement, css, html} from 'lit';
+import {customElement, state} from 'lit/decorators.js';
+import {map} from 'lit/directives/map.js';
 
 type Task = {
   id: Number;
@@ -24,19 +24,41 @@ export class ToDoListElement extends LitElement {
     ];
   }
 
+  static override styles = css`
+    .completed {
+      text-decoration-line: line-through;
+      color: #777;
+    }
+  `;
+
   protected override render(): unknown {
     return html`
       <h1>ToDo List</h1>
       ${map(this._tasks, (task) => {
         return html`
-          <li>
+          <li class=${task.completed ? 'completed' : ''}>
             <label>
-              <input type="checkbox" ?checked=${task.completed} />
+              <input
+                type="checkbox"
+                ?checked=${task.completed}
+                @click=${(event: Event) => this.setChecked(task, event)}
+              />
               ${task.label}
             </label>
           </li>
         `;
       })}
     `;
+  }
+
+  setChecked(task: Task, event: Event) {
+    const {target} = event;
+    if (!(target instanceof HTMLInputElement)) {
+      throw new TypeError('target が HTMLInputElement ではない');
+    }
+    // データ更新
+    task.completed = target.checked;
+    // DOMへの再描画を依頼
+    this.requestUpdate();
   }
 }
